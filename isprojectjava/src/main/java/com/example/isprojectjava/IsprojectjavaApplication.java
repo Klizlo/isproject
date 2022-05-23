67b0c7c261ff7bcc698986388089f197fe640679
Klizlo <klizlo0111@gmail.com>
2022-05-23 10:36:25 +0200

    Initial commit

---
+++ isprojectjava/src/main/java/com/example/isprojectjava/IsprojectjavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.isprojectjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsprojectjavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsprojectjavaApplication.class, args);
+	}
+
+}
