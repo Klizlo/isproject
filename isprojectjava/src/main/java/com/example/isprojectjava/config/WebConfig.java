6afcffc1cd78c29219d54ee92c3402f9492c34af
Klizlo <klizlo0111@gmail.com>
2022-06-04 16:17:42 +0200

    Enable Cors globally part 5

--- isprojectjava/src/main/java/com/example/isprojectjava/config/WebConfig.java
+++ isprojectjava/src/main/java/com/example/isprojectjava/config/WebConfig.java
@@ -1,19 +1,25 @@
 package com.example.isprojectjava.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
-public class WebConfig implements WebMvcConfigurer {
+public class WebConfig {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedHeaders("*");
+    @Bean
+    public WebMvcConfigurer getWebMvcConfigurer(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*");
+            }
+        };
     }
 
 
